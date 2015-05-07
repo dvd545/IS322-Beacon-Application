9e35cd83d8f4234cd93bfa270b904ba4dfb48536
David Schmidt <schmidt@DavidScmidtsMBP.home>
2015-05-07 18:27:18 -0400

    beacon and flyer services

--- conference/www/js/services.js
+++ conference/www/js/services.js
@@ -1,7 +1,23 @@
 angular.module('starter.services', ['ngResource'])
 
+    .service('BeaconService', function(Beacon){
+        var beaconsInformation = [];
+
+       var getBeaconInfo = function(){
+           console.log()
+           return beaconsInformation;
+
+       }
+        return {
+            getBeaconInfo: getBeaconInfo
+        };
+
+    })
+
     .service('FlyerService', function (PouchDBListener) {
         var flyers1 = [];
+        var adds1 = [];
+        var storeName = "target";
 
         var addFlyer = function(flyer) {
             console.log('confirmed push')
@@ -17,21 +33,32 @@ angular.module('starter.services', ['ngResource'])
             }
         }
 
+        var getAdds = function(){
+            for(var i = 0; i < flyers1.length; i++) {
+                if(flyers1[i].store === "target") {
+                    console.log('this advertisement belongs to target')
+                    adds1.push(flyers1[i]);
+                }
+            }
+            return adds1
+        }
+
         var getFlyers = function(){
-            console.log('just returned all flyers')
+
+            //it wont work in this function :/
             return flyers1;
         }
 
         var getFlyer = function(flyerId){
             for(var i = 0; i < flyers1.length; i++) {
                 if(flyers1[i]._id === flyerId) {
-                    console.log('just returned a flyer')
                     return flyers1[i];
                 }
             }
         }
 
         return {
+            getAdds: getAdds,
             addFlyer: addFlyer,
             deleteFlyer: deleteFlyer,
             getFlyers: getFlyers,
