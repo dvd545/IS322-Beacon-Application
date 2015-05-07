0598aa2d9cc1b049436037bb6c1a876418e78a20
David Schmidt <schmidt@DavidScmidtsMBP.home>
2015-05-07 18:25:40 -0400

    beacon ranging works unable to monitor beacon

--- conference/www/js/app.js
+++ conference/www/js/app.js
@@ -4,44 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var localDB = new PouchDB('beacons');
-var remoteDB = new PouchDB('http://54.149.42.95:5984/beacons', {cache : false});
+var localDB = new PouchDB('ads');
+var remoteDB = new PouchDB('http://54.149.42.95:5984/ads', {cache : false});
 
 angular.module('starter', ['ionic', 'starter.controllers'])
-
-.factory("Beacon", function(){
-        var beacon = {};
-
-        beacon.beaconRegion = null;
-        beacon.delegate = null;
-        beacon.status = 0;
-
-        beacon.helloWorld = function(){
-            console.log("HELLO WORLD!");
-        };
-
-        beacon.startedMonitoring = function(plugin){
-
-        };
-
-        beacon.getStatus = function(){
-            return beacon.status;
-        };
-
-        beacon.didEnterRegion = function(plugin){
-            console.log("Hello, from beacon!");
-            beacon.status = 1;
-        };
-
-        beacon.didExitRegion = function(plugin){
-            console.log("Goodbye, from beacon!");
-            beacon.status = 0;
-        };
-
-        return beacon;
-    })
-
-.factory('PouchDBListener', ['$rootScope', function($rootScope) {
+    .factory('PouchDBListener', ['$rootScope', function($rootScope) {
         localDB.changes({
             live: true,
             continuous: true,
@@ -66,7 +33,45 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         return true;
 
     }])
+    .factory("Beacon", function(){
+        var beacon = {};
 
+        beacon.beaconRegion = null;
+        beacon.delegate = null;
+        beacon.status = 0;
+
+        beacon.helloWorld = function(){
+            console.log("HELLO WORLD!");
+        };
+
+        beacon.startedMonitoring = function(plugin){
+            console.log("I am inside of the beacon monitoring");
+
+        };
+
+        beacon.getStatus = function(){
+            return beacon.status;
+        };
+
+        beacon.didstartRanging = function(plugin){
+            console.log("attempting to find plugin beacon data");
+            console.log(plugin.beacons[5]);
+
+
+        };
+
+        beacon.didEnterRegion = function(plugin){
+            console.log("Hello, from beacon!");
+            beacon.status = 1;
+        };
+
+        beacon.didExitRegion = function(plugin){
+            console.log("Goodbye, from beacon!");
+            beacon.status = 0;
+        };
+
+        return beacon;
+    })
 
 
 .run(function(Beacon,$ionicPlatform) {
@@ -84,78 +89,100 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 
 
+
 var delegate = new cordova.plugins.locationManager.Delegate();
 
-      delegate.didDetermineStateForRegion = function (pluginResult) {
+delegate.didDetermineStateForRegion = function (pluginResult) {
 
-          cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
-          + JSON.stringify(pluginResult));
-      };
+    cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+        + JSON.stringify(pluginResult));
+};
 
-      delegate.didStartMonitoringForRegion = function (pluginResult) {
-          Beacon.startedMonitoring(pluginResult);
-      };
+delegate.didStartMonitoringForRegion = function (pluginResult) {
+    console.log(JSON.stringify(pluginResult));
+    console.log("attempting to monitor");
+    Beacon.startedMonitoring(pluginResult);
+};
 
-      delegate.didRangeBeaconsInRegion = function (pluginResult) {
-          console.log("In Region");
-          console.log(JSON.stringify(pluginResult));
-      };
+delegate.didRangeBeaconsInRegion = function (pluginResult) {
+    console.log("In Region");
+    console.log(JSON.stringify(pluginResult));
+    Beacon.didstartRanging(pluginResult);
+};
 
-      delegate.didEnterRegion = function(pluginResult) {
-          Beacon.didEnterRegion(pluginResult);
-      };
+delegate.didEnterRegion = function(pluginResult) {
+    console.log("entered region");
+    Beacon.didEnterRegion(pluginResult);
+};
 
-      delegate.didExitRegion = function(pluginResult){
-          Beacon.didExitRegion(pluginResult);
-      };
+delegate.didExitRegion = function(pluginResult){
+    Beacon.didExitRegion(pluginResult);
+};
 
-      var uuid = '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6'; // mandatory
-      var identifier = 'flyer'; // mandatory
-      var minor = 1; // optional, defaults to wildcard if left empty
-      var major = 1; // optional, defaults to wildcard if left empty
+var uuid = '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6'; // mandatory
+var identifier = 'radbeacon'; // mandatory
+//var minor = 0; // optional, defaults to wildcard if left empty
+//var major = 0; // optional, defaults to wildcard if left empty
 
-      // throws an error if the parameters are not valid
-      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid);
+// throws an error if the parameters are not valid
+var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid);
 
-      cordova.plugins.locationManager.setDelegate(delegate);
+cordova.plugins.locationManager.setDelegate(delegate);
 
+cordova.plugins.locationManager.requestWhenInUseAuthorization();
 
+cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+    .fail(console.error)
+    .done();
 
-      cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
-          .fail(console.error)
-          .done();
+/*
+ cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
+ .fail(console.error)
+ .done();
+*/
+    });
 
-      /*
-       cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
-       .fail(console.error)
-       .done();
-       */
-  });
 
 })
-
 .controller('control',function($scope,Beacon,$interval){
-        /*
-         $scope.button = {
-         text: "Start Monitoring",
-         status: 0, //0 for off, 1 for on
-         icon: "ion-eye",
-         };
-         */
+    /*
+     $scope.button = {
+     text: "Start Monitoring",
+     status: 0, //0 for off, 1 for on
+     icon: "ion-eye",
+     };
+     */
 
 
-        $scope.showAd = false;
+    $scope.showAd = false;
 
-        $scope.checkBeacon = function(){
-            if(Beacon.getStatus() == 1){
-                $scope.showAd = true;
-            }
-            else{
-                $scope.showAd = false;
-            }
-        };
+    $scope.checkBeacon = function(){
+        if(Beacon.getStatus() == 1){
+            $scope.showAd = true;
+        }
+        else{
+            $scope.showAd = false;
+        }
+    };
 
+    $interval(function(){$scope.checkBeacon();}, 2000);
+    /*
+     $scope.pushButton = function(){
+     if($scope.button.status == 0){
+     $scope.button.status = 1;
+     $scope.button.text = "Stop Monitoring";
+     $scope.button.icon = "ion-eye-disabled";
+
+     //start beacon
+     }else{
+     $scope.button.status = 0;
+     $scope.button.text = "Start Monitoring";
+     $scope.button.icon = "ion-eye";
+     }
+     };
+     */
 })
+
 
 
 
@@ -164,48 +191,40 @@ var delegate = new cordova.plugins.locationManager.Delegate();
  .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+      .state('app', {
+          url: "/app",
+          abstract: true,
+          templateUrl: "templates/menu.html",
+          controller: 'AppCtrl'
+      })
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
+      .state('app.flyers', {
+          url: "/adds",
+          views: {
+              'menuContent': {
+                  templateUrl: "templates/adds.html",
+                  controller: 'FlyersCtrl'
+              }
+          }
+      })
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.flyers', {
-      url: "/adds",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/adds.html",
-          controller: 'FlyersCtrl'
-        }
-      }
-    })
-
-  .state('app.flyer', {
-    url: "/adds/:flyerId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/add.html",
-        controller: 'FlyerCtrl'
-      }
-    }
+      .state('app.flyer', {
+          url: "/adds/:flyerId",
+          views: {
+              'menuContent': {
+                  templateUrl: "templates/add.html",
+                  controller: 'FlyerCtrl'
+              }
+          }
+          })
+              .state('app.test', {
+                  url: "/sessions",
+                  views: {
+                      'menuContent': {
+                          templateUrl: "templates/sessions.html",
+                          controller: 'control'
+                      }
+                  }
   });
   // if none of the above states are matched, use this as the fallback
  $urlRouterProvider.otherwise('/app/adds');
